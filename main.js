a82db7e13a9494edeb2208592a390abde0ed8425
yonghan <wangyh.92@gmail.com>
2020-11-06 12:08:02 +0800

    before task8 without styling

--- main.js
+++ main.js
@@ -2,12 +2,19 @@
 const express = require('express')
 const hbs = require('express-handlebars')
 const mysql = require('mysql2/promise')
+const fetch = require("node-fetch")
+const withQuery = require("with-query").default
 
 //config PORT
 const PORT = parseInt(process.argv[2]) || parseInt(process.env.PORT) || 3000
 
 //SQL
 const SQL_SELECT_BOOKS_ALPHABET = 'select * from book2018 WHERE title LIKE ? limit ? offset ?'
+const SQL_FIND_BY_BOOK_ID = 'select * from book2018 where book_id = ?'
+
+// reviews
+const URL = 'https://api.nytimes.com/svc/books/v3/reviews.json';
+const API_KEY = process.env.API_KEY || '';
 
 // configure connection pool
 const pool = mysql.createPool({
@@ -24,6 +31,9 @@ const pool = mysql.createPool({
 const app = express()
 app.engine('hbs', hbs({ defaultLayout: 'default.hbs' }))
 app.set('view engine', 'hbs')
+
+app.use(express.static(__dirname + '/static')) 
+app.use(express.urlencoded({extended: true}))
 
 const startApp = async(app,pool) => {
     try{
@@ -66,7 +76,7 @@ app.get("/books/:letter",async(req,res)=>{
 
     try {
         let result = await conn.query(SQL_SELECT_BOOKS_ALPHABET,[`${req.params.letter}%`, limit, offset])
-        console.log(result[0]);
+        // console.log(result[0]);
         res.status(200)
         res.type('text/html')
         res.render('books',{
@@ -83,4 +93,68 @@ app.get("/books/:letter",async(req,res)=>{
     }
 })
 
+app.get("/book/:bookid",async(req,res)=>{
+    // console.log(req.params.bookid);
+    const conn = await pool.getConnection()
+
+    try {
+    
+        /************************************/
+        let result = await conn.query(SQL_FIND_BY_BOOK_ID, [req.params.bookid])
+        // console.log(result[0][0]);
+        res.status(200)
+        res.type('text/html')
+        res.render('bookDetails',{
+            book: result[0][0],
+        })
+    } catch (e) {
+        console.log(e);
+    } finally {
+        conn.release()
+    }
+})
+
+app.post('/searchReview',express.urlencoded({extended: true}), async(req,res)=>{
+    // console.log(req.body);
+
+    const url = withQuery(
+        URL,
+        {
+            author: req.body.author,
+            title: req.body.title,
+            "api-key": API_KEY,
+        }
+    )
+
+    // console.log(url);
+    
+    let resultURL = await fetch(url)
+    
+    let jsResult = await resultURL.json();
+    
+    // console.log(jsResult.results);
+
+    const bookReview = jsResult.results
+    .map((e)=>{
+        return {
+            title: e.book_title, 
+            author: e.book_author, 
+            reviewer: e.byline,
+            reviewDate: e.publication_dt,
+            summary: e.summary,
+            reviewUrl: e.url
+        }
+    })
+
+    console.log(bookReview);
+
+    res.status(200)
+    res.type("text/html")
+    res.render('bookReview', {
+        bookReview,
+        empty: jsResult.results.length <=0
+    })
+})
+
 startApp(app,pool);
+
